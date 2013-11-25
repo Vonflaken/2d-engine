e342f14e657de83aefd573897913aa484de9ccf7
Ignacio Pascual <vonflaken@gmail.com>
2013-11-25 15:45:47 +0100

    working on add npot texture compatibility ( wip )

--- src/src/image.cpp
+++ src/src/image.cpp
@@ -27,15 +27,34 @@ Image::Image(const String &filename, uint16 hframes, uint16 vframes) {
 	unsigned char *newBuffer;
 	if ( !isPOTWidth || !isPOTHeight )
 	{
-		// Set newBuffer pixel not needed to white
-		width = pow( ceil( Log2( ( double ) width ) ), 2 );
-		height = pow( ceil( Log2( ( double ) height ) ), 2 );
+		// Set buffer data on a new resized buffer
+		
+		int newWidth = width;
+		int newHeight = height;
+		unsigned const int bufferSize = width * height * 4;
 
-		newBuffer = new unsigned char[ width * height * 4 ];
+		newBuffer = ( unsigned char * ) malloc( bufferSize );
+		memcpy( newBuffer, buffer, bufferSize );
+		// Check sizes to change
+		if ( !isPOTWidth )
+		{
+			newWidth = pow( floor( Log2( ( double ) width ) ), 2 );
+			// Set U coor to new ratio based on new width
+			lastU = ( ( double ) newWidth ) / width;
+		}
+		if ( !isPOTHeight )
+		{
+			newHeight = pow( floor( Log2( ( double ) height ) ), 2 );
+			// Set V coor to new ratio based on new height
+			lastV = ( ( double ) newHeight ) / height;
+		}
+		
+		buffer = NULL; // stbi_image_free( buffer );
 	}
 	// Generamos la textura
 	if ( buffer ) {
 		// TAREA: Generar la textura de OpenGL
+		// Set POT texture
 		glGenTextures( 1, &gltex );
 		glBindTexture( GL_TEXTURE_2D, gltex );
 		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
@@ -43,6 +62,17 @@ Image::Image(const String &filename, uint16 hframes, uint16 vframes) {
 		glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer );
 
 		stbi_image_free( buffer );
+	}
+	else if ( newBuffer )
+	{
+		// Set POTed texture
+		glGenTextures( 1, &gltex );
+		glBindTexture( GL_TEXTURE_2D, gltex );
+		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+		glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, newBuffer );
+
+		newBuffer = NULL;
 	}
 }
 
