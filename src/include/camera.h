7f39b66a975d8f9eea95ef7c1ea2137d4a747d5f
Ignacio Pascual <vonflaken@gmail.com>
2013-12-12 16:33:00 +0100

    camera files fix

--- src/include/camera.h
+++ src/include/camera.h
@@ -1,6 +1,7 @@
 #ifndef UGINE_CAMERA_H
 #define UGINE_CAMERA_H
 
+#include "screen.h"
 #include "math.h"
 #include <stdio.h>
 
@@ -10,20 +11,19 @@ class Camera
 {
 public:
 	Camera();
-	// Camera( double x, double y, Sprite * sprite = NULL, double xy0 = 0.00, double by0 = 0.00, double xy1 = 0.00, double by1 = 0.00 );
 
-	virtual void SetPosition( double x, double y ) { this->x = x; this->y = y; }
-	virtual void SetX( double x ) { this->x = x; }
-	virtual void SetY( double y ) { this->y = y; }
+	virtual void SetPosition( double x, double y ) { SetX( x ); SetY( y ); }
+	virtual void SetX( double x );
+	virtual void SetY( double y );
 	virtual double GetX() const { return x; }
 	virtual double GetY() const { return y; }
 
-	virtual void SetBounds( double bx0, double by0, double bx1, double by1 ) { boundx0 = bx0; boundy0 = by0; boundx1 = bx1; boundy1 = by1; }
+	virtual void SetBounds( double bx0, double by0, double bx1, double by1 ) { boundx0 = bx0; boundy0 = by0; boundx1 = bx1; boundy1 = by1; SetPosition( GetX(), GetY() ); }
 	virtual bool HasBounds() const { if ( ( boundx0 - boundx1 ) <= EPSILON ) { return false; } return true; }
 	virtual double GetMinX() const { return boundx0; }
 	virtual double GetMinY() const { return boundy0; }
-	virtual double GetMaxX() const { return boundx1; }
-	virtual double GetMaxY() const { return boundy1; }
+	virtual double GetMaxX() const { return boundx1 - Screen::Instance().GetWidth(); }
+	virtual double GetMaxY() const { return boundy1 - Screen::Instance().GetHeight(); }
 
 	virtual void FollowSprite( Sprite * sprite ) { followingSprite = sprite; }
 
--- src/src/camera.cpp
+++ src/src/camera.cpp
@@ -1,6 +1,5 @@
 #include "../include/camera.h"
 #include "../include/sprite.h"
-#include "../include/screen.h"
 
 Camera::Camera()
 {
@@ -12,40 +11,58 @@ Camera::Camera()
 	boundy1 = 0.00;
 	followingSprite = NULL;
 }
-/*
-Camera::Camera( double x, double y, Sprite * sprite, double bx0, double by0, double bx1, double by1 )
-{
-	this->x = x;
-	this->y = y;
-	followingSprite = sprite;
-	boundx0 = bx0;
-	boundy0 = by0;
-	boundx1 = bx1;
-	boundy1 = by1;
-}
-*/
-void Camera::Update()
-{
-	if ( followingSprite != NULL )
-	{
-		// Follow sprite
-		x = followingSprite->GetX() - Screen::Instance().GetWidth() / 2;
-		y = followingSprite->GetY() - Screen::Instance().GetHeight() / 2;
-	}
 
-	if ( HasBounds() )
+void Camera::SetX( double x )
+{
+	if ( !HasBounds() )
 	{
-		// Check limits
-		if ( x > boundx0 && y > boundy0 && 
-			x < boundx1 - Screen::Instance().GetWidth() && y < boundy1 - Screen::Instance().GetHeight() )
+		this->x = x;
+	}
+	else
+	{
+		if ( x < GetMinX() )
 		{
-			// Inside
-			Screen::Instance().SetTitle( "Dentro de los límites" );
+			this->x = GetMinX();
+		}
+		else if ( x > GetMaxX() )
+		{
+			this->x = GetMaxX();
 		}
 		else
 		{
-			// Outside
-			Screen::Instance().SetTitle( "Fuera de los límites" );
+			this->x = x;
 		}
+	}
+}
+
+void Camera::SetY( double y )
+{
+	if ( !HasBounds() )
+	{
+		this->y = y;
+	}
+	else
+	{
+		if ( y < GetMinY() )
+		{
+			this->y = GetMinY();
+		}
+		else if ( y > GetMaxY() )
+		{
+			this->y = GetMaxY();
+		}
+		else
+		{
+			this->y = y;
+		}
+	}
+}
+
+void Camera::Update()
+{
+	if ( followingSprite )
+	{
+		SetPosition( followingSprite->GetScreenX() - Screen::Instance().GetWidth() / 2, 
+			followingSprite->GetScreenY() - Screen::Instance().GetHeight() / 2 );
 	}
 }
